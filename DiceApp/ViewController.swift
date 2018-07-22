9804c1c7413f116a10c8124eb47e67b8e98e9d4c
Harsh <deadspaceharsh@gmail.com>
2018-07-22 18:25:07 +0530

    started diceapp

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by Harsh Mehta on 22/07/18.
+//  Copyright © 2018 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
